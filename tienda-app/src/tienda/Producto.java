3f72ad915078b4c39c5c40fa27a3b0c3552b5208
JPC9999 <153563111+JPC9999@users.noreply.github.com>
2024-05-22 21:06:47 -0500

    Actualizar Producto.java

--- tienda-app/src/tienda/Producto.java
+++ tienda-app/src/tienda/Producto.java
@@ -6,7 +6,45 @@ public class Producto {
     private double precio;
     private int cantidad;
 
-    public void guardar() {
-      
+    public Producto(String nombre, double precio, int cantidad) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+
+    public double calcularValorTotal() {
+        return precio * cantidad;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Precio: $" + precio);
+        System.out.println("Cantidad: " + cantidad);
+        System.out.println("Valor Total: $" + calcularValorTotal());
     }
 }
